390775b9cc474636474bb5a9a2821e9c0139df5e
dev-hk22 <dev17@scube.net.in>
2026-07-10 13:38:06 +0530

    expo-app setup

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -23,7 +23,7 @@ export const STORAGE_KEYS = {
 }
 
 export const TITLES = {
-    APP_NAME: 'KS Institute of Technology',
+    APP_NAME: 'Auro',
     SHOW_POWER_BY: false,
     SHOW_REMOVE_ACCOUNT: true,
     SHOW_ANSWER_BOOKLET: false
