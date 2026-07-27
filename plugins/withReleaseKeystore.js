48c9e31eb3cf7a025b8241e30e20f4daf9b6157b
dev-hk22 <dev17@scube.net.in>
2026-07-27 11:41:00 +0530

    fixes

--- plugins/withReleaseKeystore.js
+++ plugins/withReleaseKeystore.js
@@ -1,10 +1,9 @@
-import { withAppBuildGradle, withGradleProperties } from '@expo/config-plugins';
+const { withAppBuildGradle, withGradleProperties } = require('@expo/config-plugins');
 
 const withReleaseKeystore = (config) => {
   // 1. Inject gradle.properties values
   config = withGradleProperties(config, (config) => {
     const props = config.modResults;
-
     const setProp = (key, value) => {
       const existing = props.find((p) => p.key === key);
       if (existing) {
@@ -14,10 +13,10 @@ const withReleaseKeystore = (config) => {
       }
     };
 
-    setProp('MYAPP_RELEASE_STORE_FILE', '../../keystores/my-release-key-keystore');
-    setProp('MYAPP_RELEASE_KEY_ALIAS', process.env.RELEASE_KEY_ALIAS || '');
-    setProp('MYAPP_RELEASE_STORE_PASSWORD', process.env.RELEASE_STORE_PASSWORD || '');
-    setProp('MYAPP_RELEASE_KEY_PASSWORD', process.env.RELEASE_KEY_PASSWORD || '');
+    setProp('MYAPP_RELEASE_STORE_FILE', '../../keystores/my-release-key.keystore');
+    setProp('MYAPP_RELEASE_KEY_ALIAS', process.env.EXPO_PUBLIC_RELEASE_KEY_ALIAS || '');
+    setProp('MYAPP_RELEASE_STORE_PASSWORD', process.env.EXPO_PUBLIC_RELEASE_STORE_PASSWORD || '');
+    setProp('MYAPP_RELEASE_KEY_PASSWORD', process.env.EXPO_PUBLIC_RELEASE_KEY_PASSWORD || '');
 
     return config;
   });
