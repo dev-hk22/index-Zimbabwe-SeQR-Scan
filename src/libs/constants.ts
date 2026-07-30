c635ce92e1fed9c1baddb2faa0f62310f5783a22
dev-hk22 <dev17@scube.net.in>
2026-07-30 10:33:44 +0530

    expo-app-upgrade

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -23,7 +23,7 @@ export const STORAGE_KEYS = {
 }
 
 export const TITLES = {
-    APP_NAME: 'GHRCEMP',
+    APP_NAME: 'GHRICACSN',
     SHOW_POWER_BY: false,
     SHOW_REMOVE_ACCOUNT: true,
     SHOW_ANSWER_BOOKLET: false
