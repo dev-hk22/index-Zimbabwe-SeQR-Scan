dff9cfe86779c26c92df8ebd67541ad1b5ff5223
dev-hk22 <dev17@scube.net.in>
2026-07-31 16:37:56 +0530

    expo-app-upgrade-setup

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -23,7 +23,7 @@ export const STORAGE_KEYS = {
 }
 
 export const TITLES = {
-    APP_NAME: 'ICAT',
+    APP_NAME: 'IIT Jammu',
     SHOW_POWER_BY: false,
     SHOW_REMOVE_ACCOUNT: true,
     SHOW_ANSWER_BOOKLET: false
