0b9cc3ca50c545bd62ca90925efc6b57bf3d9181
dev-hk22 <dev17@scube.net.in>
2026-08-04 17:17:11 +0530

    expo-app-upgrade-setup

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -23,7 +23,7 @@ export const STORAGE_KEYS = {
 }
 
 export const TITLES = {
-    APP_NAME: 'JNTUACEK',
+    APP_NAME: 'JSSAHER',
     SHOW_POWER_BY: false,
     SHOW_REMOVE_ACCOUNT: true,
     SHOW_ANSWER_BOOKLET: false
