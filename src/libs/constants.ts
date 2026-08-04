fe7b61b3ec158f4df2a0435037ac0a0e1b0e581f
dev-hk22 <dev17@scube.net.in>
2026-08-04 15:16:31 +0530

    expo-app-upgrade-setup

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -23,7 +23,7 @@ export const STORAGE_KEYS = {
 }
 
 export const TITLES = {
-    APP_NAME: 'Ismail Yusuf College',
+    APP_NAME: 'JNTUACEK',
     SHOW_POWER_BY: false,
     SHOW_REMOVE_ACCOUNT: true,
     SHOW_ANSWER_BOOKLET: false
