a9dd8e61de87e666766ac2dfbc6e05d5750b9ec3
dev-hk22 <dev17@scube.net.in>
2026-08-19 17:55:13 +0530

    expo-app-upgrade

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -23,7 +23,7 @@ export const STORAGE_KEYS = {
 }
 
 export const TITLES = {
-    APP_NAME: 'Nidan Technologies',
+    APP_NAME: 'PBCE',
     SHOW_POWER_BY: false,
     SHOW_REMOVE_ACCOUNT: true,
     SHOW_ANSWER_BOOKLET: false
