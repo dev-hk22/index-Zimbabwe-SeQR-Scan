8173dfd238ff7546e0cea99c2ed777758fea4ba6
dev-hk22 <dev17@scube.net.in>
2026-08-20 13:22:42 +0530

    Expo-app-upgrade

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -23,7 +23,7 @@ export const STORAGE_KEYS = {
 }
 
 export const TITLES = {
-    APP_NAME: 'PCE',
+    APP_NAME: 'PJLCP',
     SHOW_POWER_BY: false,
     SHOW_REMOVE_ACCOUNT: true,
     SHOW_ANSWER_BOOKLET: false
