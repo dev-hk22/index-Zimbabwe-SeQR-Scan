ef27bf5f821c32e38a77c948ae88329be923ae31
dev-hk22 <dev17@scube.net.in>
2026-08-24 12:14:23 +0530

    expo-app-upgrade

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -23,7 +23,7 @@ export const STORAGE_KEYS = {
 }
 
 export const TITLES = {
-    APP_NAME: 'print24x7',
+    APP_NAME: 'SECURA',
     SHOW_POWER_BY: false,
     SHOW_REMOVE_ACCOUNT: true,
     SHOW_ANSWER_BOOKLET: false
