8be1ae022c087a13568969d7838bd24f6f754944
dev-hk22 <dev17@scube.net.in>
2026-08-24 16:50:13 +0530

    expo-app-upgrade

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -23,7 +23,7 @@ export const STORAGE_KEYS = {
 }
 
 export const TITLES = {
-    APP_NAME: 'SECURA',
+    APP_NAME: 'SGRSA',
     SHOW_POWER_BY: false,
     SHOW_REMOVE_ACCOUNT: true,
     SHOW_ANSWER_BOOKLET: false
