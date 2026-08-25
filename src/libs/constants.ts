376ab7823940b393108b55a7087ea2803835bcb1
dev-hk22 <dev17@scube.net.in>
2026-08-25 17:40:48 +0530

    initial-app-setup

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -23,7 +23,7 @@ export const STORAGE_KEYS = {
 }
 
 export const TITLES = {
-    APP_NAME: 'SIU',
+    APP_NAME: 'Index',
     SHOW_POWER_BY: false,
     SHOW_REMOVE_ACCOUNT: true,
     SHOW_ANSWER_BOOKLET: false
